e32bbefecb593c8d6e07df12733d73650c4c81c2
shiivankiit <shivankit7557@gmail.com>
2025-08-07 09:27:00 +0530

    Star pattern assignment

---
+++ Assignment/week2-assignment/Numeric-Hollow-inverted-half-pyramid.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<n-row;col++)
+        {
+            cout<<n+(row-1);
+        }
+        cout<<endl;
+    }
+
+
+
+
+}
